4e5dcdea4fe8ed4f45deb96dccb35627dfeab01f
osamaelshaer <osamaelshaer944@gmail.com>
2022-02-24 15:19:33 +0200

    substitute User model Abstractuser

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.models import User
-from .models import Profile
+from .models import  Profile , User
 
 
 
 
 
 admin.site.register(Profile)
+admin.site.register(User)
+
 
 
 '''
